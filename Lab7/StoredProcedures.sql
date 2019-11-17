82b0b29cb111ec79f15cd16b585632711c631557
Denys Shutka <shine.at.dusk@gmail.com>
2019-11-17 15:26:51 +0200

    added stored procedures

--- Lab7/StoredProcedures.sql
+++ Lab7/StoredProcedures.sql
@@ -31,3 +31,52 @@ BEGIN
 END
 //
 DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE copyRegion()
+BEGIN
+
+DECLARE done int DEFAULT false;
+DECLARE region_name, region_code VARCHAR(45);
+DECLARE new_region_table1 VARCHAR(45) DEFAULT CONCAT("region", CURRENT_TIMESTAMP());
+DECLARE new_region_table2 VARCHAR(45) DEFAULT CONCAT("region", DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL 1 SECOND));
+
+DECLARE region_cursor CURSOR
+FOR SELECT name, code FROM `region`;
+DECLARE CONTINUE HANDLER
+FOR NOT FOUND SET done = true;
+
+OPEN region_cursor;
+
+SET @temp_query=CONCAT("CREATE TABLE IF NOT EXISTS `", new_region_table1, 
+	"` (`name` VARCHAR(45) NOT NULL, `code` VARCHAR(45) NOT NULL, PRIMARY KEY (`name`))");
+PREPARE myquery FROM @temp_query;
+EXECUTE myquery;
+DEALLOCATE PREPARE myquery;
+
+SET @temp_query=CONCAT("CREATE TABLE IF NOT EXISTS `", new_region_table2, 
+	"` (`name` VARCHAR(45) NOT NULL, `code` VARCHAR(45) NOT NULL, PRIMARY KEY (`name`))");
+PREPARE myquery FROM @temp_query;
+EXECUTE myquery;
+DEALLOCATE PREPARE myquery;
+
+myLoop: LOOP
+
+	FETCH region_cursor INTO region_name, region_code;
+
+	IF done=true THEN LEAVE myLoop;
+	END IF;
+
+	SET @temp_query=CONCAT("INSERT INTO `", 
+		IF(FLOOR(RAND() * 10) % 2 <> 0, new_region_table1, new_region_table2), 
+        "` (`name`, `code`) VALUES ('", region_name, "', '", region_code, "')");
+
+	PREPARE myquery FROM @temp_query;
+	EXECUTE myquery;
+	DEALLOCATE PREPARE myquery;
+
+END LOOP;
+CLOSE region_cursor;
+
+END //
+DELIMITER ;
